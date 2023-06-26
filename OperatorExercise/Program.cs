30ec75bd8773932fbf66a0b6fd5115f72fe88284
chris mcmains <mcmainsc84@gmail.com>
2023-06-26 15:57:27 -0400

    completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            int c = 5;
+            int d = 10;
+
+            int addition = c + d;
+            int subtraction = c - d;
+            int multiplication = c * d;
+            int division = c / d;
+            int modulus = c % d;
+
+            Console.WriteLine("What is the radius of the circle");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle with the radius of {radius} is: " + area);
+        
+        static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
+        // Thought Exercise : K = 16
+
     }
+}
 }
